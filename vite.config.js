b2d7d27041ba3528aee8479be0f22e648409dc64
Prabal <gillprabal@gmail.com>
2025-04-04 22:32:07 +1300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/6210_30056643/', // This is your repository name in the URL
+  plugins: [react()]
+});
